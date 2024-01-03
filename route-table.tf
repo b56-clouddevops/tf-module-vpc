0445357c431666abf5413394a054809d2b6dcadd
CodingManoj <CodingManoj@gmail.com>
2024-01-03 07:26:18 +0530

    TF VPC Auto Push

--- route-table.tf
+++ route-table.tf
@@ -1,5 +1,4 @@
 # Creates Public Route Table
-
 resource "aws_route_table" "public_rt" {
   vpc_id = aws_vpc.main.id
 
@@ -11,4 +10,12 @@ resource "aws_route_table" "public_rt" {
   tags = {
     Name = "roboshop-${var.ENV}-public-rt"
   }
+}
+
+# Public Subnet Association 
+resource "aws_route_table_association" "public_subnet)rt_association" {
+  count          = length(aws_subnet.public_subnet.*.id)
+
+  subnet_id      = element(aws_subnet.public_subnet.*.id, count.index)
+  route_table_id = aws_route_table.public_rt.id
 }
