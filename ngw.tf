c263b9e27a1d4eb7b5d2a8d357fe0a0c1884327d
CodingManoj <CodingManoj@gmail.com>
2024-01-03 07:34:14 +0530

    TF VPC Auto Push

---
+++ ngw.tf
@@ -0,0 +1,18 @@
+# Reserves an elstic ip
+resource "aws_eip" "aws_eip" {
+  domain         = "vpc"
+  tags = {
+    Name         = "roboshop-${var.ENV}-ngw-eip"
+  }
+}
+
+resource "aws_nat_gateway" "ngw" {
+  allocation_id = aws_eip.ngw_eip.id
+  subnet_id     = aws_subnet.public_subnet.*.id[0]
+
+  tags = {
+    Name = "roboshop-${var.ENV}-ngw"
+  }
+
+  depends_on = [aws_internet_gateway.igw, aws_eip.aws_eip]
+}
