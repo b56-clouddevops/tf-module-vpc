a140b1de00bafb24de0bd363f45ab5609cf4e266
CodingManoj <CodingManoj@gmail.com>
2024-01-04 06:13:32 +0530

    TF VPC Auto Push

--- route-table.tf
+++ route-table.tf
@@ -26,8 +26,8 @@ resource "aws_route_table" "private_subnet" {
   vpc_id = aws_vpc.main.id
 
   route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw.id
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.ngw.id
   }
 
   tags = {
