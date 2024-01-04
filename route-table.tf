ed69c3e2265e5591f1b0ff581f5e957d2652f452
CodingManoj <CodingManoj@gmail.com>
2024-01-04 06:23:20 +0530

    TF VPC Auto Push

--- route-table.tf
+++ route-table.tf
@@ -32,7 +32,7 @@ resource "aws_route_table" "private_subnet" {
 
   route {
     cidr_block                = var.DEFAULT_VPC_CIDR
-    vpc_peering_connection_id = ????
+    vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
   }
 
   tags = {
