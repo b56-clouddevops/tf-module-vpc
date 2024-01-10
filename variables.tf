0c89bc8cf6c66a6d60304134034307287a366d67
CodingManoj <CodingManoj@gmail.com>
2024-01-10 06:53:57 +0530

    TF DB Auto Push

--- variables.tf
+++ variables.tf
@@ -8,3 +8,10 @@ variable "PRIVATE_SUBNET_CIDR" {}
 variable "DEFAULT_VPC_CIDR" {}
 variable "DEFAULT_VPC_ID" {}
 variable "DEFAULT_VPC_RT" {}
+
+# Hosted Zone Variables
+variable "PUBLIC_HOSTED_ZONE_ID" {}
+variable "PUBLIC_HOSTED_ZONE_NAME" {}
+
+variable "PRIVATE_HOSTED_ZONE_ID" {} 
+variable "PRIVATE_HOSTED_ZONE_NAME" {}
