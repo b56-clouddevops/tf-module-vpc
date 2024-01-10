9244bbe094fee8c542941ce7bb4f1efee46908df
CodingManoj <CodingManoj@gmail.com>
2024-01-10 07:19:40 +0530

    TF DB Auto Push

--- peering.tf
+++ peering.tf
@@ -4,4 +4,3 @@ resource "aws_vpc_peering_connection" "peer" {
   peer_vpc_id   = aws_vpc.main.id
   auto_accept   = true                            # This works only if both the VPC's are in the same account.
 }
-
