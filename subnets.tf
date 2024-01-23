af23b5702f8b088aa6e782018493bd415c6fbc42
CodingManoj <CodingManoj@gmail.com>
2024-01-22 19:35:02 -0500

    TF Backend Infra Auto Push

--- subnets.tf
+++ subnets.tf
@@ -1,9 +1,10 @@
 resource "aws_subnet" "public_subnet" {
-  count              = length(var.PUBLIC_SUBNET_CIDR)
+  count                     = length(var.PUBLIC_SUBNET_CIDR)
 
-  vpc_id             = aws_vpc.main.id
-  cidr_block         = element(var.PUBLIC_SUBNET_CIDR, count.index)
-  availability_zone  = element(var.AZ, count.index)
+  vpc_id                    = aws_vpc.main.id
+  cidr_block                = element(var.PUBLIC_SUBNET_CIDR, count.index)
+  availability_zone         = element(var.AZ, count.index)
+  map_public_ip_on_launch   = true
 
   tags = {
     Name = "roboshop-${var.ENV}-public-subnet-${count.index+1}"
